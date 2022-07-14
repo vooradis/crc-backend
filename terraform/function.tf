f9233cfc88e9841516ce5b9aa201709e6499b30b
Samrat Vooradi <106648385+vooradis@users.noreply.github.com>
2022-07-14 12:17:29 +1000

    Update function.tf

--- terraform/function.tf
+++ terraform/function.tf
@@ -1,55 +1,16 @@
-# Generates an archive of the source code compressed as a .zip file.
-data "archive_file" "source" {
-    type        = "zip"
-    source_dir  = "../src"
-    output_path = "/tmp/function.zip"
-}
-
-# Add source code zip to the Cloud Function's bucket
-resource "google_storage_bucket_object" "zip" {
-    source       = data.archive_file.source.output_path
-    content_type = "application/zip"
-
-    # Append to the MD5 checksum of the files's content
-    # to force the zip to be updated as soon as a change occurs
-    name         = "src-${data.archive_file.source.output_md5}.zip"
-    bucket       = google_storage_bucket.function_bucket.name
-
-    # Dependencies are automatically inferred so these lines can be deleted
-    depends_on   = [
-        google_storage_bucket.function_bucket,  # declared in `storage.tf`
-        data.archive_file.source
-    ]
-}
-
-# Create the Cloud function triggered by a `Finalize` event on the bucket
 resource "google_cloudfunctions_function" "function" {
-    name                  = "function-trigger-on-gcs"
-    runtime               = "python37"  # of course changeable
+  name        = "function-test"
+  description = "My function"
+  runtime     = "python37"
 
-    # Get the source code of the cloud function as a Zip compression
-    source_archive_bucket = google_storage_bucket.function_bucket.name
-    source_archive_object = google_storage_bucket_object.zip.name
-
-    # Must match the function name in the cloud function `main.py` source code
-    entry_point           = "visitor_count"
-    
-    trigger_http          = true
-    
-    #
-    #event_trigger {
-    #    event_type = "google.storage.object.finalize"
-    #    resource   = "${var.project_id}-input"
-    #}
-    
-    # Dependencies are automatically inferred so these lines can be deleted
-    depends_on            = [
-        google_storage_bucket.function_bucket,  # declared in `storage.tf`
-        google_storage_bucket_object.zip
-    ]
+  available_memory_mb   = 128
+  source_archive_bucket = google_storage_bucket.bucket.name
+  source_archive_object = google_storage_bucket_object.archive.name
+  trigger_http          = true
+  entry_point           = "visitor_count"
 }
 
-# Create IAM entry so all users can invoke the function
+# IAM entry for all users to invoke the function
 resource "google_cloudfunctions_function_iam_member" "invoker" {
   project        = google_cloudfunctions_function.function.project
   region         = google_cloudfunctions_function.function.region
