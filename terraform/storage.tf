a5911e05b8b285c67a1b1f72fe594685ef2c97bb
Samrat Vooradi <106648385+vooradis@users.noreply.github.com>
2022-07-15 22:41:03 +1000

    Delete terraform directory

--- terraform/storage.tf
+++
@@ -1,9 +0,0 @@
-resource "google_storage_bucket" "function_bucket" {
-    name     = "${var.project_id}-function"
-    location = var.region
-}
-
-resource "google_storage_bucket" "input_bucket" {
-    name     = "${var.project_id}-input"
-    location = var.region
-}
