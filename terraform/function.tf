d5b257a15f0266cd657138c5d37b36239a7fe38d
Samrat Vooradi <106648385+vooradis@users.noreply.github.com>
2022-07-24 21:16:40 +1000

    Update function.tf

--- terraform/function.tf
+++ terraform/function.tf
@@ -35,6 +35,44 @@ resource "google_cloudfunctions_function" "function" {
   entry_point           = "visitor_count"
 }
 
+resource "google_api_gateway_api" "api_gw" {
+  provider     = google-beta
+  api_id       = local.api_gateway_container_id
+  project      = var.project_id
+  display_name = local.display_name
+}
+
+resource "google_api_gateway_api_config" "api_cfg" {
+  provider             = google-beta
+  api                  = google_api_gateway_api.api_gw.api_id
+  api_config_id_prefix = local.api_config_id_prefix
+  project              = var.project_id
+  display_name         = local.display_name
+
+  openapi_documents {
+    document {
+      path     = "openapi.yaml"
+      contents = filebase64("openapi.yml")
+    }
+  }
+  lifecycle {
+    create_before_destroy = true
+  }
+}
+
+resource "google_api_gateway_gateway" "gw" {
+  provider = google-beta
+  region   = var.region
+  project  = var.project_id
+  
+  api_config   = google_api_gateway_api_config.api_cfg.id
+
+  gateway_id   = local.gateway_id
+  display_name = local.display_name
+  
+  depends_on   = [google_api_gateway_api_config.api_cfg]
+}
+
 # IAM entry for all users to invoke the function
 resource "google_cloudfunctions_function_iam_member" "invoker" {
   project        = google_cloudfunctions_function.function.project
